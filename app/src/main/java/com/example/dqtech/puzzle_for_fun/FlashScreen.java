5b2cc6b4847fe88542f95aa7e957ce67cf91a7ea
Guru <gururajbijjaragi@gmail.com>
2018-01-23 10:45:49 +0530

    drawable

--- app/src/main/java/com/example/dqtech/puzzle_for_fun/FlashScreen.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/FlashScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class FlashScreen extends AppCompatActivity {
 
+    //hello
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
