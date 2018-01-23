68934032e85d2988830ed134f00f61b4973b9b9b
venkatkilaparthi <venkat.kilaparthi.7@gmail.com>
2018-01-23 11:09:23 +0530

    image

--- app/src/main/java/com/example/dqtech/puzzle_for_fun/FlashScreen.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/FlashScreen.java
@@ -1,13 +1,45 @@
 package com.example.dqtech.puzzle_for_fun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class FlashScreen extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_screen);
+
+        image = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.zoom);
+        image.startAnimation(myanim);
+
+        final Intent i=new Intent(this,Login.class);
+        Thread timer = new Thread(){
+            public void run () {
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+
+
+            }
+
+        };
+        timer.start();
+
+
     }
 }
