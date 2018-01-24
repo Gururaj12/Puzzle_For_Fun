17eeaba71b434725a0010b5b8e2a71b63543a51a
ranjith <ranju0515@gmail.com>
2018-01-24 13:45:59 +0530

    home and 10 gk actv

--- app/src/main/java/com/example/dqtech/puzzle_for_fun/Home.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/Home.java
@@ -2,13 +2,27 @@ package com.example.dqtech.puzzle_for_fun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+    Button b1,b2,b3,b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //hi
+        b1= (Button) findViewById(R.id.b1);
+        b2= (Button) findViewById(R.id.b2);
+        b3= (Button) findViewById(R.id.b3);
+        b4= (Button) findViewById(R.id.b4);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"4pics in 1 answer",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
