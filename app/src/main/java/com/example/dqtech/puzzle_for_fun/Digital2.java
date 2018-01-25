c1637f32a5eaa193fa3f565c1b482067731d0fa5
ranjith <ranju0515@gmail.com>
2018-01-25 13:48:09 +0530

    home and 10 gk actv jmk v

---
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/Digital2.java
@@ -0,0 +1,17 @@
+package com.example.dqtech.puzzle_for_fun;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ADMIN on 1/25/2018.
+ */
+
+public class Digital2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digital2);
+    }
+}
--- app/src/main/java/com/example/dqtech/puzzle_for_fun/Seven.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/Seven.java
@@ -3,7 +3,7 @@ package com.example.dqtech.puzzle_for_fun;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class seven extends AppCompatActivity {
+public class Seven extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
