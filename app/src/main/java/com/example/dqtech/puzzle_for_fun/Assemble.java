4da12b6e75d6c513684b3cd3df3414dd7d7f389b
Himaja <dinunanik@gmail.com>
2018-01-29 12:54:28 +0530

    food & digital

--- app/src/main/java/com/example/dqtech/puzzle_for_fun/Assemble.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/Assemble.java
@@ -9,5 +9,6 @@ public class Assemble extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assemble);
+        //awer
     }
 }
