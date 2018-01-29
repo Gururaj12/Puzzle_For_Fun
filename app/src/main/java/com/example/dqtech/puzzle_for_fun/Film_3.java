a1ea61cd63a9b9a274ffef0aec5bc1720e9dfd0f
venkatkilaparthi <venkat.kilaparthi.7@gmail.com>
2018-01-29 10:04:07 +0530

    7 film

--- app/src/main/java/com/example/dqtech/puzzle_for_fun/Film_3.java
+++ app/src/main/java/com/example/dqtech/puzzle_for_fun/Film_3.java
@@ -1,20 +1,13 @@
 package com.example.dqtech.puzzle_for_fun;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class IQ_Test_1 extends AppCompatActivity {
-
-
+public class Film_3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iq__test_1);
-
+        setContentView(R.layout.activity_film_3);
     }
 }
